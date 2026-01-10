7ce8419eb0325aea6f40e3a585fccd93b5eff0b6
DoHyeon Lee <ldh106582@naver.com>
2026-01-11 07:21:36 +0900

    cors 수정

--- web.js
+++ web.js
@@ -57,7 +57,7 @@ app.use(cors({
     origin: [
         'https://exam-website-fe211.web.app',
         'https://exam-website-fe211.firebaseapp.com',
-        'examwebsite-1993.duckdns.org',
+        'https://examwebsite-1993.duckdns.org',
         '134.185.117.189'
     ],
     credentials: true
